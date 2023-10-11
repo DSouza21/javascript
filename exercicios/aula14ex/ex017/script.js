c330bbe9e8e454fb5b157b3c3cf8cbb373052799
Damião V. Souza <139881408+damiaovieiraa@users.noreply.github.com>
2023-10-11 10:29:38 -0300

    Update script.js

--- exercicios/aula14ex/ex017/script.js
+++ exercicios/aula14ex/ex017/script.js
@@ -1,59 +1,36 @@
 function contar() {
     var inicio = document.getElementById('iinicio').value
-    var i = Number(inicio)
     var fim = document.getElementById('ifim').value
-    var f = Number(fim)
     var passo = document.getElementById('ipasso').value
-    var p = Number(passo)
     var res = document.getElementById('res')   
-
     if (inicio.length == 0 || fim.length == 0 || passo.length == 0) {
         res.innerHTML = 'Impossível contar!'
     } else {
-        if (p == 0) {
+        var i = Number(inicio)
+        var f = Number(fim)
+        var p = Number(passo)
+        if (p <= 0) {
             window.alert('Passo inválido! Considerando PASSO 1')
-            if (p == 0 && i > f) {
-                p = 1 
-                res.innerHTML = ''
-                for (var c = i; c >= f; c -= p) {
-                    res.innerHTML += `${c} &#x1F449;`
-                }
-                res.innerHTML += `&#x1F3C1;`
-            } else if (p == 0 && i < f) {
-                p = 1 
-                res.innerHTML = ''
-                for (var c = i; c <= f; c += p) {
-                    res.innerHTML += `${c} &#x1F449;`
-                }
-                res.innerHTML += `&#x1F3C1;`
-            } else {
-                p = 1 
-                res.innerHTML = ''
-                for (var c = i; c <= f; c += p) {
-                    res.innerHTML += `${c} &#x1F449;`
-                }
-                res.innerHTML += `&#x1F3C1;`
+            p = 1 
+        }
+        if (i > f) {
+            // Contagem regressiva
+            res.innerHTML = ''
+            for (var c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} &#x1F449;`
             }
-        } else {
-            if (i > f) {
-                res.innerHTML = ''
-                for (var c = i; c >= f; c -= p) {
-                    res.innerHTML += `${c} &#x1F449;`
-                }
-                res.innerHTML += `&#x1F3C1;`
-            } else if (i < f) {
-                res.innerHTML = ''
-                for (var c = i; c <= f; c += p) {
-                    res.innerHTML += `${c} &#x1F449;`
-                }
-                res.innerHTML += `&#x1F3C1;`
-            } else { 
-                res.innerHTML = ''
-                for (var c = i; c <= f; c += p) {
-                    res.innerHTML += `${c} &#x1F449;`
-                }
-                res.innerHTML += `&#x1F3C1;`
+        } else if (i < f) {
+            // Contagem crescente
+            res.innerHTML = ''
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += `${c} &#x1F449;`
+            }
+        } else { 
+            res.innerHTML = ''
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += `${c} &#x1F449;`
             }
         }
+        res.innerHTML += `\u{1F3C1}`
     }
-}
+} 
