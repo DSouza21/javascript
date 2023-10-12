4b2802a1394a822bbf6a09878669e805921f91a5
Damião V. Souza <139881408+damiaovieiraa@users.noreply.github.com>
2023-10-12 16:13:56 -0300

    criei cinco exercícios de função

---
+++ exercicios/aula16/funcao01.js
@@ -0,0 +1,13 @@
+function parimpar(n) {
+    if (n % 2 == 0) {
+        return 'PAR!'
+    } else {
+        return 'ÍMPAR!'
+    }
+}
+/*
+let res = parimpar(2)
+console.log(res) 
+*/
+
+console.log(parimpar(3)) 
